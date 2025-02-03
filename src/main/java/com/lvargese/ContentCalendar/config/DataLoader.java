c94a9707c7c6ac24ab1ba1adb5cc80b73a09319f
Liyons Varghese <liyonsvarghese@gmail.com>
2025-02-03 16:11:49 +0530

    Profiles

--- src/main/java/com/lvargese/ContentCalendar/config/DataLoader.java
+++ src/main/java/com/lvargese/ContentCalendar/config/DataLoader.java
@@ -1,14 +1,18 @@
 package com.lvargese.ContentCalendar.config;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+// Restricting this beans to a profile .We can do  ("!production) as well .
+//can include or exclude this
 //CommandLine Runner will run even if it isn't called in any other files.
+@Profile("dev")
 @Component
 public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Hello World");
+        System.out.println("Hello xD");
     }
 }
